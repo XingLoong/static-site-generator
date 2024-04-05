090b7ff465deab1afdfa3eb7d603daeea9c6d676
XingLoong <xing.loong@gmail.com>
2024-04-06 01:31:04 +1100

    CH2.4 LeafNodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase): 
   def test_to_html(self):
@@ -26,3 +26,18 @@ class TestHTMLNode(unittest.TestCase):
       prop2.props_to_html(),
       ' div="something" href="https://boot.dev"'
     )
+  
+  def test_leaf_no_children_no_prop(self):
+    node = LeafNode("p", "Hello World!")
+    self.assertEqual(node.to_html(), "<p>Hello World!</p>")
+
+  def test_leaf_no_tag(self):
+    node = LeafNode(None, "Hello World!")
+    self.assertEqual(node.to_html(), "Hello World!")
+
+  def test_repr(self):
+    node = LeafNode("p", "Hi")
+    self.assertEqual(repr(node), "LeafNode(p, Hi, None)")
+
+if __name__ == "__main__":
+    unittest.main()
