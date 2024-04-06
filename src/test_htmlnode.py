11113a0e1fc05f725a3acc408dc749677be60a70
XingLoong <xing.loong@gmail.com>
2024-04-06 12:14:12 +1100

    CH2.5 ParentNode/Recursion

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase): 
   def test_to_html(self):
@@ -35,9 +35,33 @@ class TestHTMLNode(unittest.TestCase):
     node = LeafNode(None, "Hello World!")
     self.assertEqual(node.to_html(), "Hello World!")
 
-  def test_repr(self):
+  def test_leaf_repr(self):
     node = LeafNode("p", "Hi")
     self.assertEqual(repr(node), "LeafNode(p, Hi, None)")
+
+  def test_parent_child(self):
+    child_node = LeafNode("p", "child")
+    parent_node = ParentNode("div", [child_node])
+    self.assertEqual(parent_node.to_html(), "<div><p>child</p></div>")
+
+  def test_parent_grandchild(self):
+    granchild_node = LeafNode("b", "grandchild")
+    child_node = ParentNode("p", [granchild_node])
+    parent_node = ParentNode("div", [child_node])
+    self.assertEqual(parent_node.to_html(), "<div><p><b>grandchild</b></p></div>")
+
+  def test_many_grandchildren_eg(self):
+    node = ParentNode(
+      "p",
+      [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+      ]
+    )
+    self.assertEqual(node.to_html(), 
+                     "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
 
 if __name__ == "__main__":
     unittest.main()
