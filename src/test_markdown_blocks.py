f82e382449f844a75ab5b21e14e32c6ff3edab3b
XingLoong <xing.loong@gmail.com>
2024-04-26 01:58:06 +1000

    4.2 block types

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,14 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+  markdown_to_blocks,
+  block_to_block_type,
+  block_type_paragraph,
+  block_type_heading,
+  block_type_code,
+  block_type_quote,
+  block_type_ulist,
+  block_type_olist
+)
 
 class TestMarkdown(unittest.TestCase):
   def test_markdown(self):
@@ -44,6 +53,19 @@ This is the same paragraph on a new line
         "* This is a list\n* with items",
       ],
     )
+
+  def test_block_to_block_types(self):
+    block = "## heading"
+    self.assertEqual(block_to_block_type(block), block_type_heading)
+    block = "```\mcode\n```"
+    self.assertEqual(block_to_block_type(block), block_type_code)
+    block = "> quote\n> quote 2\n> quote 3"
+    self.assertEqual(block_to_block_type(block), block_type_quote)
+    block = "* list\n* list 2"
+    self.assertEqual(block_to_block_type(block), block_type_ulist)
+    block = "1. first\n2. second\n3. third"
+    self.assertEqual(block_to_block_type(block), block_type_olist)
+
 
 if __name__ == "__main__":
   unittest.main()
